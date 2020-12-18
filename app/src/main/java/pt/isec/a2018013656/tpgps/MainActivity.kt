e91132adfccaeb623eb8e01ab573eef002d58a28
AlexPBSJ <alexstarminecraft2000@gmail.com>
2020-12-18 22:44:42 +0000

    -

---
+++ app/src/main/java/pt/isec/a2018013656/tpgps/MainActivity.kt
@@ -0,0 +1,11 @@
+package pt.isec.a2018013656.tpgps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
