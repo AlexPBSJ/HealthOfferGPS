f8b337bf38b587b592e6897b9b58755d73c68b48
João <Romo>
2020-12-19 14:45:14 +0000

    Tentativa Perfil

--- app/src/main/java/pt/isec/a2018013656/tpgps/MainActivity.kt
+++ app/src/main/java/pt/isec/a2018013656/tpgps/MainActivity.kt
@@ -1,11 +1,19 @@
 package pt.isec.a2018013656.tpgps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onVisualizaPerfil(view: View) {
+        val intent = Intent(this, PerfilActivity::class.java)
+        startActivity(intent)
+    }
+
 }
