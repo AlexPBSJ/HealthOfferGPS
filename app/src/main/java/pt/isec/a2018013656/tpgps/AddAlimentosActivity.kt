c75165a8bed139e0ceccfaad3f482c3e32288ee5
AlexPBSJ <alexstarminecraft2000@gmail.com>
2020-12-28 21:31:40 +0000

    spinner das categorias e ficha de alimentos

--- app/src/main/java/pt/isec/a2018013656/tpgps/AddAlimentosActivity.kt
+++ app/src/main/java/pt/isec/a2018013656/tpgps/AddAlimentosActivity.kt
@@ -1,11 +1,37 @@
 package pt.isec.a2018013656.tpgps
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class AddAlimentosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_alimentos_layout)
+
+        val categorias = resources.getStringArray(R.array.categorias_array)
+
+        val spinner: Spinner = findViewById(R.id.spinner_categorias)
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item, categorias
+        )
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View, position: Int, id: Long
+            ) {
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // write code to perform some action
+            }
+        }
     }
 }
